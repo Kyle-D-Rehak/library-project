edef563378905edc60150ca37b3e9c898e7e2dfa
Kyle Rehak <kylerehak@gmail.com>
2022-12-02 15:43:26 +0800

    Refactor code to class design pattern

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ const cancelButton = document.getElementById('cancelButton');
 const form = document.getElementById('form');
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor (title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 function addBookToLibrary(title, author, pages, read) {
